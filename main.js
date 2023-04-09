a29bb7fe280fea5b99da270e23f62cfb715b9ee2
William Gispiela <william.gispiela@gmail.com>
2023-04-09 20:18:50 -0300

    "initial commit"

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  $("#carousel-imagens").slick({
+    autoplay: true,
+  });
+
+  $(".menu-hamburger").click(function () {
+    $("nav").slideToggle();
+  });
+
+  $("#telefone").mask("(00) 00000-0000");
+
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+      },
+      mensagem: {
+        required: false,
+      },
+    },
+    messages: {
+      nome: "Por favor, insira o seu nome",
+    },
+    submitHandler: function (form) {
+      console.log(form);
+    },
+    invalidHandler: function (evento, validador) {
+      let camposIncorretos = validador.numberOfInvalids();
+      if (camposIncorretos) {
+        alert(`Existem ${camposIncorretos} campos incorretos`);
+      }
+    },
+  });
+
+  $("html").animate(
+    {
+      scrollTop: destino.offset().top,
+    },
+    1000
+  );
+});
